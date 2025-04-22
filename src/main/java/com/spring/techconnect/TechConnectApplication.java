dedad948e0ef4e33de580d2c7abb784e8db84fbe
patangerohit <rohitrpatange2003@gmail.com>
2025-04-22 15:49:07 +0530

    Project Setup

---
+++ src/main/java/com/spring/techconnect/TechConnectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.techconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/techconnect/TechConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.techconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
